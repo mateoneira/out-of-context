0a3cb897aea08e23b382ad2217cfe028ad0e1631
Mateo Neira <mateo0719@gmail.com>
2021-02-26 22:40:52 +0000

    added port

--- message.py
+++ message.py
@@ -1,22 +1,25 @@
 from flask import Flask, jsonify
 import json
-# import numpy
+import os
+import numpy
 
 app = Flask(__name__)
 
 with open('messages.json') as _file:
     messages = json.load(_file)
 
-# def select_random():
-#     return numpy.random.choice(messages)
+def select_random():
+    return numpy.random.choice(messages)
 
 @app.route("/")
 def start():
-    response = jsonify('hello')
+    response = jsonify(select_random())
     return response
 
     
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host = '0.0.0.0', port)
     app.run()
 
